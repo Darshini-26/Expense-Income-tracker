919ca92d9db931465237c6b0a9e6eacf6b461df5
Darshini Shanmugam <darshinishanmugam26@gmail.com>
2024-10-29 19:37:59 +0530

    Create income

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi import Depends
 from src.database import SessionLocal, engine
 import src.models as models
 
@@ -6,10 +7,19 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Dependency
+
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
+
+@app.post("/income/", response_model=Income)
+def create_income(income: IncomeCreate, db: Session = Depends(get_db)):
+    db_income = Income(**income.dict())
+    db.add(db_income)
+    db.commit()
+    db.refresh(db_income)
+    return db_income
