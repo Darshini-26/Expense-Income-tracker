176b0ee713322292dd96c6f24f6472b1055794cf
Darshini Shanmugam <darshinishanmugam26@gmail.com>
2024-10-30 12:38:27 +0530

    fixed bugs

--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 from fastapi import Depends
 from src.database import SessionLocal, engine
 import src.models as models
-
+import src.schemas as schemas
+from sqlalchemy.orm import Session
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -16,9 +17,9 @@ def get_db():
         db.close()
 
 
-@app.post("/income/", response_model=Income)
-def create_income(income: IncomeCreate, db: Session = Depends(get_db)):
-    db_income = Income(**income.dict())
+@app.post("/income/", response_model=schemas.Income)
+def create_income(income: schemas.IncomeCreate, db: Session = Depends(get_db)):
+    db_income = models.Income(**income.dict())
     db.add(db_income)
     db.commit()
     db.refresh(db_income)
