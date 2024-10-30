985cd3af3342948cbd567d55d457a2fe6a59eb04
Darshini Shanmugam <darshinishanmugam26@gmail.com>
2024-10-30 17:23:45 +0530

    endpoints for create

--- src/schemas.py
+++ src/schemas.py
@@ -1,12 +1,12 @@
 from pydantic import BaseModel
 from datetime import datetime
-from typing import Optional
+from typing import Optional, Union
 
 class IncomeBase(BaseModel):
     user_id: int
     income_amt: float
     date: datetime
-    description: Optional[str] = None
+    description: Optional[Union[str,None]] = None
     account_id: int
     category_id: int
 
@@ -19,6 +19,25 @@ class Income(IncomeBase):
     class Config:
         orm_mode = True
 
+
+class ExpenseBase(BaseModel):
+    user_id: int
+    expense_amt: float
+    date: datetime
+    description: Optional[str] = None
+    account_id: int
+    category_id: int
+
+class ExpenseCreate(ExpenseBase):
+    pass
+
+class Expense(ExpenseBase):
+    expense_id: int
+
+    class Config:
+        orm_mode = True
+
+
 class BankAccountBase(BaseModel):
     user_id: int
     balance: float
@@ -30,6 +49,20 @@ class BankAccountCreate(BankAccountBase):
 
 class BankAccount(BankAccountBase):
     account_id: int
+
+    class Config:
+        orm_mode = True
+        
+
+class CategoryBase(BaseModel):
+    user_id: int
+    category_type: str  
+
+class CategoryCreate(CategoryBase):
+    pass
+
+class Category(CategoryBase):
+    category_id: int
 
     class Config:
         orm_mode = True
