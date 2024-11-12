1d1b46cd8943ad02f5fa38b492a302827de20c58
Python Women <darshinishanmugam26@gmail.com>
2024-11-13 00:01:25 +0530

    endpoints for expense

--- src/main.py
+++ src/main.py
@@ -57,3 +57,39 @@ def delete_income(income_id:int , db: Session = Depends(get_db)):
     return {"Message":"ID not available"}
 
 
+@app.post("/expense/", response_model=schemas.Expense)
+def create_expense(expense: schemas.ExpenseCreate, db: Session = Depends(get_db)):
+    db_expense = models.Expense(**expense.dict())
+    db.add(db_expense)
+    db.commit()
+    db.refresh(db_expense)
+    return db_expense
+
+
+@app.get("/expense/", response_model=List[schemas.Expense])
+def read_expense(db: Session = Depends(get_db)):
+    db_expense = db.query(models.Expense).all()
+    if not db_expense:
+        return []
+    return db_expense
+
+@app.put("/expense/{expense_id}", response_model=schemas.Expense)
+def update_expense(expense_id: int, expense: schemas.ExpenseCreate, db: Session = Depends(get_db)):
+    db_expense = db.query(models.Expense).filter(models.Expense.expense_id==expense_id).first()
+    db_expense.user_id = expense.user_id
+    db_expense.expense_amt = expense.expense_amt
+    db_expense.description = expense.description
+    db_expense.account_id = expense.account_id
+    db_expense.category_id = expense.category_id
+    db.commit()
+    db.refresh(db_expense)
+    return db_expense
+
+@app.delete("/expense/{expense_id}")
+def delete_expense(expense_id:int , db: Session = Depends(get_db)):
+    db_expense = db.query(models.Expense).filter(models.Expense.expense_id==expense_id).first()
+    if db_expense:
+        db.delete(db_expense)
+        db.commit()
+        return {"Message":"Records deleted"}
+    return {"Message":"ID not available"}
