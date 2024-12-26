ad8ab861a354cd524bb4ceaf820d298ef5822ee6
Python Women <darshinishanmugam26@gmail.com>
2024-12-26 23:41:17 +0530

    Added docstrings

--- src/schemas.py
+++ src/schemas.py
@@ -1,108 +1,111 @@
-
-from pydantic import BaseModel, EmailStr, root_validator
 from datetime import datetime
 from typing import Optional, Union
+from pydantic import BaseModel, EmailStr, root_validator
 import uuid
 
+
+# User Models
 class UserBase(BaseModel):
     name: str
     email: EmailStr
 
+
 class UserCreate(UserBase):
     pass
 
+
 class User(UserBase):
     user_id: uuid.UUID
-    name: str
-    email: str
 
     class Config:
         from_attributes = True
 
+
+# Income Models
 class IncomeBase(BaseModel):
-    # user_id: int
     income_amt: float
     date: datetime
-    description: Optional[Union[str,None]] = None
+    description: Optional[Union[str, None]] = None
     account_id: int
-    category_id:int= 1
+    category_id: int = 1
+
 
 class IncomeCreate(BaseModel):
-    email: EmailStr
+    name_of_bank: str
     income_amt: float
     date: datetime
-    description: Optional[Union[str,None]] = None
-    account_id: int
-    category_id: int=1
+    description: Optional[Union[str, None]] = None
+    category_id: int = 1
+
 
 class Income(IncomeBase):
     income_id: int
-    #user_id: uuid.UUID
 
     class Config:
         from_attributes = True
 
 
+# Expense Models
 class ExpenseBase(BaseModel):
-    # user_id: int
     expense_amt: float
     date: datetime
     description: Optional[str] = None
     account_id: int
-    category_id:int= 2
+    category_id: int = 2
+
 
 class ExpenseCreate(BaseModel):
-    email: EmailStr
+    name_of_bank: str
     expense_amt: float
     date: datetime
     description: Optional[str] = None
-    account_id: int
-    category_id: int=2
+    category_id: int = 2
+
 
 class Expense(ExpenseBase):
     expense_id: int
-    #user_id: uuid.UUID 
 
     class Config:
         from_attributes = True
 
 
+# Bank Account Models
 class BankAccountBase(BaseModel):
-    # user_id: int
     balance: float
     name_of_bank: str
-    
+
+
 class BankAccountCreate(BaseModel):
     balance: float
     name_of_bank: str
     email: str
-   
+
+
 class BankAccount(BankAccountBase):
     account_id: int
-    #account_no:int
-    #user_id: uuid.UUID
 
     class Config:
         from_attributes = True
-        
 
+
+# Category Models
 class CategoryBase(BaseModel):
-    # user_id: int
-    category_type: str  
+    category_type: str
+
 
 class CategoryCreate(BaseModel):
-    category_type: str 
+    category_type: str
 
     @root_validator(pre=True)
     def check_category_type(cls, values):
-        category_type = values.get('category_type')
+        category_type = values.get("category_type")
         if category_type not in ["Income", "Expense"]:
             raise ValueError("category_type must be either 'Income' or 'Expense'")
         return values
+
 
 class Category(CategoryBase):
     category_id: int
 
     class Config:
         from_attributes = True
-
